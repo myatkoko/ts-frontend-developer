98b77a91477d7edeb922270409d2f1f821c06d13
Myat Ko Ko <30685131+myatkoko@users.noreply.github.com>
2019-09-06 12:51:25 +0800

    Update solution_b.js

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -4,5 +4,11 @@
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 var sum_to_n = function(n) {
-    // your code here
+    do{
+        var sum = 0;
+        var index = 0;
+        while(index < n){
+            sum += index;
+        }
+    }
 };
